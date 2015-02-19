3823e40777156048b40813804a9bf9a94faad5b3
J.Marx <jennyrutledge@gmail.com>
2015-02-19 16:33:43 -0500

    Complete ROT13Cipher module.

--- app/js/rot13.js
+++ app/js/rot13.js
@@ -2,4 +2,52 @@
 
 var ROT13Cipher = (function () {
   // your code here
+  var Code = function(key){
+
+    var _charToCode = function(letter)
+    {
+      var asciiId = letter.charCodeAt(0) + key;
+      if(asciiId <= 122){
+        return String.fromCharCode(asciiId);
+      }else{
+        return String.fromCharCode(asciiId - 26);
+      };
+    };
+
+    var _codeToChar = function(letter)
+    {
+      var asciiId = letter.charCodeAt(0) - key;
+      if(asciiId >= 97){
+        return String.fromCharCode(asciiId);
+      }else{
+        return String.fromCharCode(asciiId + 26);
+      };
+    };
+
+    var encode = function(str){
+      var arr = str.split("");
+
+      var out = arr.map(_charToCode);
+      return out.join("");
+    };
+
+    var decode = function(str){
+      var arr = str.split("");
+
+      var out = arr.map(_codeToChar);
+      return out.join("");
+    };
+
+    return {
+    key: key,
+    encode: encode,
+    decode:decode
+    };
+  };
+
+  return{
+    Code: Code
+  };
+
 })();
+
--- test/spec/rot13_spec.js
+++ test/spec/rot13_spec.js
@@ -1,10 +1,19 @@
 'use strict';
 
 describe('ROT13Cipher', function () {
+  var cipher = ROT13Cipher.Code(13);
   describe('.encode', function () {
-    // write your test here
+    it('tests the encode',function(){
+      expect(cipher.encode("hello")).toEqual('uryyb');
+      expect(cipher.encode("jeff")).toEqual('wrss');
+      expect(cipher.encode("abcdefghijklmnopqrstuvwxyz")).toEqual('nopqrstuvwxyzabcdefghijklm');
+    });
   });
   describe('.decode', function () {
-    // write your test here
+    it('tests the decode',function(){
+    expect(cipher.decode("uryyb")).toEqual('hello');
+    expect(cipher.decode("wrss")).toEqual('jeff');
+    expect(cipher.decode("nopqrstuvwxyzabcdefghijklm")).toEqual('abcdefghijklmnopqrstuvwxyz');
+    });
   });
 });
