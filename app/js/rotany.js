33ebdfab8b82d1c52518e044ace368fcc3212ad8
Anthony Broccoli <acbrocco@syr.edu>
2015-02-19 17:45:00 -0500

    got rotate any working

--- app/js/rotany.js
+++ app/js/rotany.js
@@ -33,14 +33,14 @@ var ROTANYCipher = (function() {
       for(var i=0; i<string.length; i++){
        value = array[i].charCodeAt();
         if (value >= 'A'.charCodeAt() && value <= 'Z'.charCodeAt()){
-          if (value-num > 'Z'.charCodeAt()){
-           shift = value - (num - 1) - ('Z'.charCodeAt() - 'A'.charCodeAt());
+          if (value-num < 'A'.charCodeAt()){
+           shift = value - (num - 1) + ('Z'.charCodeAt() - 'A'.charCodeAt());
           }else{
            shift = value - num;
           }
         }else if(( value >= 'a'.charCodeAt() && value <= 'z'.charCodeAt() )) {
-          if (value-num > 'z'.charCodeAt()){
-           shift = value - (num - 1) - ('z'.charCodeAt() - 'a'.charCodeAt());
+          if (value-num < 'a'.charCodeAt()){
+           shift = value - (num - 1) + ('z'.charCodeAt() - 'a'.charCodeAt());
           }else{
            shift = value - num;
           }
