df710c4478d8a741762335a2cf3127b49910f2cb
Max Blaushild <blaushmild@gmail.com>
2015-02-19 17:52:30 -0500

    finishes everything including bonuses

--- test/spec/rot13_spec.js
+++ test/spec/rot13_spec.js
@@ -1,10 +1,24 @@
-'use strict';
-
 describe('ROT13Cipher', function () {
-  describe('.encode', function () {
-    // write your test here
-  });
-  describe('.decode', function () {
-    // write your test here
-  });
+ var cipher = ROT13Cipher;
+
+ describe('.encode', function () {
+   it('encodes the phrase "hello"', function () {
+     expect(cipher.encode('hello', 13)).toEqual('uryyb');
+   });
+ });
+ describe('.decode', function () {
+   it('decodes the phrase "uryyb"', function () {
+     expect(cipher.decode('uryyb', 13)).toEqual('hello');
+   });
+ });
+ describe('.encode', function () {
+   it('encodes the phrase "hello dad"', function () {
+     expect(cipher.encode('hellodad', 13)).toEqual('uryybqnq');
+   });
+ });
+ describe('.decode', function () {
+   it('decodes the phrase "uryyb qnq"', function () {
+     expect(cipher.decode('uryybqnq', 13)).toEqual('hellodad');
+   });
+ });
 });
