540e1f09bead3c7fdeced8e600a70cd025aa27d1
Anthony Broccoli <acbrocco@syr.edu>
2015-02-19 17:25:51 -0500

    Created ROT13 and started ROTANY

---
+++ app/js/rotany.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var ROTANYCipher = (function() {
+    var encode = function(string, num){
+      var array = string.split(''),
+          letters = [],
+          shift, value;
+      for(var i=0; i<string.length; i++){
+       value = array[i].charCodeAt();
+        if (value >= 'A'.charCodeAt() && value <= 'Z'.charCodeAt()){
+          if (value+num > 'Z'.charCodeAt()){
+           shift = value + (num - 1) - ('Z'.charCodeAt() - 'A'.charCodeAt());
+          }else{
+           shift = value + num;
+          }
+        }else if(( value >= 'a'.charCodeAt() && value <= 'z'.charCodeAt() )) {
+          if (value+num > 'z'.charCodeAt()){
+           shift = value + (num - 1) - ('z'.charCodeAt() - 'a'.charCodeAt());
+          }else{
+           shift = value + num;
+          }
+        }else{
+          shift = value;
+        }
+       letters.push(String.fromCharCode(shift));
+      }
+      return letters.join('');
+    };
+    var decode = function(string, num){
+      var array = string.split(''),
+          letters = [],
+          shift, value;
+      for(var i=0; i<string.length; i++){
+       value = array[i].charCodeAt();
+        if (value >= 'A'.charCodeAt() && value <= 'Z'.charCodeAt()){
+          if (value-num > 'Z'.charCodeAt()){
+           shift = value - (num - 1) - ('Z'.charCodeAt() - 'A'.charCodeAt());
+          }else{
+           shift = value - num;
+          }
+        }else if(( value >= 'a'.charCodeAt() && value <= 'z'.charCodeAt() )) {
+          if (value-num > 'z'.charCodeAt()){
+           shift = value - (num - 1) - ('z'.charCodeAt() - 'a'.charCodeAt());
+          }else{
+           shift = value - num;
+          }
+        }else{
+          shift = value;
+        }
+       letters.push(String.fromCharCode(shift));
+      }
+      return letters.join('');
+    };
+    return{
+      encode: encode,
+      decode: decode
+    };
+})();
