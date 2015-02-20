cab1631bb379bd1352738b70d152bf0f3aec030f
Charles Talbot <charlietalbot@gmail.com>
2015-02-20 09:30:39 -0500

    bonuses not all done, but happy to have worked throguh this so far

--- test/spec/rot13_spec.js
+++ test/spec/rot13_spec.js
@@ -1,10 +1,29 @@
 'use strict';
 
 describe('ROT13Cipher', function () {
+     var cipher = ROT13Cipher;
+
   describe('.encode', function () {
-    // write your test here
+    it('returns the given string with each character shifted by 13'){
+      expect(cipher.encode('hello')).toEqual('uryyb');
+      expect(cipher.encode('Hello Charles')).toEqual('Uryyb Puneyrf')
+    });
   });
   describe('.decode', function () {
-    // write your test here
+it('returns the given string with each character shifted by 13'){
+      expect(cipher.encode('uryyb')).toEqual('hello');
+      expect(cipher.encode('Uryyb Puneyrf')).toEqual('Hello Charles');
+    });
   });
 });
+
+
+/*
+tests needed,
+
+turns the alphabet into an array of characters
+
+shifts the characters in place, 13 spaces
+
+
+*/
