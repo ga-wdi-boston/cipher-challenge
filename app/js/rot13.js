735347a8331f7337e2661d738e3d9ef3152529ac
idelairre <idelairre@gmail.com>
2015-02-19 20:25:47 -0500

    closing in on a solution after working on this for forever.

--- app/js/rot13.js
+++ app/js/rot13.js
@@ -1,5 +1,45 @@
 'use strict';
 
-var ROT13Cipher = (function () {
-  // your code here
+var ROT13Cipher = (function() {
+    var encode = function(stringIn) {
+        var encodedString = [];
+
+        var findCharFor = function(string) {
+            for (var i = 0; i < string.length; i++) {
+                encodedString.push(string.charCodeAt(i));
+            }
+        };
+
+        var setCharPos = function() {
+            for (var i = 0; i < encodedString.length; i++) {
+                encodedString[i] = encodedString[i] - 97;
+            }
+        };
+
+        var changeCharValues = function() {
+            for (var i = 0; i < encodedString.length; i++) {
+                if ((encodedString[i] + 13) > 26) {
+                  encodedString[i] = (encodedString[i] + 13) - 26;
+            } else {
+                encodedString[i] = encodedString[i] + 13;
+            }
+        }
+
+        var encodeString = function() {
+            for (var i = 0; i < encodedString.length; i++) {
+                // encodedString[i] = encodedString[i] + 96;
+                encodedString[i] = String.fromCharCode(i);
+                // encodedString.join("");
+            }
+        }
+
+    return {
+        encode: encode
+    }
+}
+
+// var decode
+
 })();
+
+ROT13Cipher.encode("abcdefghijklmnopqrstucvwxyz");
--- test/spec/rot13_spec.js
+++ test/spec/rot13_spec.js
@@ -2,9 +2,11 @@
 
 describe('ROT13Cipher', function () {
   describe('.encode', function () {
-    // write your test here
+    it('rotates each character in the string 13 places forward in the alphabet', function()
+      expect(ROT13Cipher.encode("abcdefghijklmnopqrstuvwxyz")).toBe("nopqrstuvwxyzabcdefghijklm");
   });
   describe('.decode', function () {
-    // write your test here
+  it('rotates each character in the string back 13 places in the alphabet', function(){
+    expect(ROT13Cipher.decode("nopqrstuvwxyzabcdefghijklm").toBe("abcdefghijklmnopqrstuvwxyz");
   });
 });
