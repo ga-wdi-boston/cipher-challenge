5e1ab0ec63cfec8d6d30f733eecc31f010e4471f
J-Weeks <weeksjasons@gmail.com>
2015-02-20 09:15:20 -0500

    homework w/tests

--- app/js/rot13.js
+++ app/js/rot13.js
@@ -1,5 +1,61 @@
 'use strict';
 
-var ROT13Cipher = (function () {
-  // your code here
+// var ROT13Cipher = (function () {
+//   var alpha = "abcdefghijklmnopqrstucvwxyz"
+//   alpha = alpha.split("")
+//   return alpha
+
+//   var goThrough = function(word){
+//     word = word.split("");
+//     wordIndex = word.map(indexOf(word))
+
+//   }
+// })();
+
+
+
+// var goThrough = function(inputString){
+//   var alpha = "abcdefghijklmnopqrstucvwxyz";
+//   alpha = alpha.split("");
+//   var inputStringArray = inputString.split("");
+//   var wordIndex = inputStringArray.map(function(ch){
+//  //   console.log(ch);
+//     var indexInAlpha = alpha.indexOf(ch);
+//     console.log("orig char is " + indexInAlpha);
+//     var newIndexAlpha = (indexInAlpha + 13) % alpha.length;
+//     console.log("new char is " + alpha[newIndexAlpha]);
+//     return  (alpha[newIndexAlpha];
+//   });
+
+//     return wordIndex
+//   };
+//   console.log(goThrough("candy"))
+
+  var ROT13Cipher = (function () {
+  var abc = 'abcdefghijklmnopqrstuvwxyz';
+  var plain = abc.split("");
+
+  var cipherAbc = 'nopqrstuvwxyzabcdefghijklm';
+  var cipher = cipherAbc.split("");
+
+  var encode = function(word) {
+    return word.split('').map(function(char) {
+      return cipher[plain.indexOf(char)]}).join('');
+  }
+
+  var decode = function(word) {
+    return word.split('').map(function(char) {
+      return plain[cipher.indexOf(char)]}).join('');
+  }
+
+  return {
+    encode: encode,
+    decode: decode
+  }
+
 })();
+
+
+console.log(ROT13Cipher.encode("hello"));
+
+
--- test/spec/rot13_spec.js
+++ test/spec/rot13_spec.js
@@ -2,9 +2,56 @@
 
 describe('ROT13Cipher', function () {
   describe('.encode', function () {
-    // write your test here
+    it('encodes a string using the caesar cipher', function() {
+      expect(ROT13Cipher.encode("hello")).toEqual('uryyb');
+    });
   });
+});
+
+describe('ROT13Cipher', function () {
+  describe('.encode', function () {
+    it('encodes a string using the caesar cipher', function() {
+      expect(ROT13Cipher.encode("awesome")).toEqual('njrfbzr');
+    });
+  });
+});
+
+describe('ROT13Cipher', function () {
   describe('.decode', function () {
-    // write your test here
+    it('decodes a string using the caesar cipher', function() {
+      expect(ROT13Cipher.decode("uryyb")).toEqual('hello');
+    });
+  });
+});
+
+describe('ROT15Cipher', function () {
+  describe('.encode', function () {
+    it('encodes a string using the caesar cipher', function() {
+      expect(ROT15Cipher.encode("hello")).toEqual('gdkkn');
+    });
+  });
+});
+
+describe('ROT15Cipher', function () {
+  describe('.encode', function () {
+    it('encodes a string using the caesar cipher', function() {
+      expect(ROT15Cipher.encode("awesome")).toEqual('zvdrnld');
+    });
+  });
+});
+
+describe('ROT15Cipher', function () {
+  describe('.decode', function () {
+    it('decodes a string using the caesar cipher', function() {
+      expect(ROT15Cipher.decode("gdkkn")).toEqual('hello');
+    });
+  });
+});
+
+describe('ROT13upcaseCipher', function () {
+  describe('.encode', function () {
+    it('encodes a string using the caesar cipher', function() {
+      expect(ROT13Cipher.encode("UPCASE")).toEqual('hcpnfr');
+    });
   });
 });
