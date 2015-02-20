c58fe12dd65b2b3c4beac3be5433f6db5f6bfa7c
Ashlinn Brennan <asbren13@gmail.com>
2015-02-19 19:05:19 -0500

    hw done, needs to be refactored still

--- app/js/rot13.js
+++ app/js/rot13.js
@@ -1,5 +1,126 @@
 'use strict';
 
 var ROT13Cipher = (function () {
-  // your code here
+  var original = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var cipher = ['n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m'];
+
+  var encode = function(string){
+    var result = [];
+    var string = string.toLowerCase().split('');
+    for (var i = 0; i < string.length; i++) {
+      var index = original.indexOf(string[i]);
+        if(index === -1){
+          result.push(string[i]);
+        } else {
+          result.push(string[i].replace(string[i], cipher[index]));
+        };
+    };
+    return result.join('');
+  };
+
+  var decode = function(string) {
+    return encode(string)
+  };
+
+  return {
+    encode: encode,
+    decode: decode
+  };
 })();
+
+
+var ROT25Cipher = (function () {
+  var original = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var cipher = ['z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y'];
+
+  var encode = function(string){
+    var result = [];
+    var string = string.toLowerCase().split('');
+    for (var i = 0; i < string.length; i++) {
+      var index = original.indexOf(string[i]);
+        if(index === -1){
+          result.push(string[i]);
+        } else {
+          result.push(string[i].replace(string[i], cipher[index]));
+        };
+    };
+    return result.join('');
+  };
+
+  var decode = function(string) {
+    var result = [];
+    var string = string.toLowerCase().split('');
+    for (var i = 0; i < string.length; i++) {
+      var index = cipher.indexOf(string[i]);
+        if(index === -1){
+          result.push(string[i]);
+        } else {
+          result.push(string[i].replace(string[i], original[index]));
+        };
+    };
+    return result.join('');
+  };
+
+  return {
+    encode: encode,
+    decode: decode
+  };
+})();
+
+var ROTnCipher = (function () {
+  var original = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  var encode = function(string, n){
+    var result = [];
+    var string = string.toLowerCase().split('');
+    for (var i = 0; i < string.length; i++) {
+      var index = original.indexOf(string[i]);
+        if(index === -1){
+          result.push(string[i]);
+        } else if (index + n > 25) {
+          index = index + n - 26;
+          result.push(string[i].replace(string[i], original[index]))
+        } else {
+          result.push(string[i].replace(string[i], original[index + n]))
+        };
+    };
+    return result.join('');
+  };
+
+  var decode = function(string, n){
+    var result = [];
+    var string = string.toLowerCase().split('');
+    for (var i = 0; i < string.length; i++) {
+      var index = original.indexOf(string[i]);
+        if(index === -1){
+          result.push(string[i]);
+        } else if (index - n < 0) {
+          index = index - n + 26;
+          result.push(string[i].replace(string[i], original[index]))
+        } else {
+          result.push(string[i].replace(string[i], original[index - n]))
+        };
+    };
+    return result.join('');
+  };
+
+  return {
+    encode: encode,
+    decode: decode
+  };
+})();
+
+// var alldecoders = function(string){
+//   for(var i = 0; i < 25; i++) {
+//     console.log(ROTnCipher.decode(string, i));
+//   }
+// };
+
+// alldecoders('string');
+
+var allencoders = function(string){
+  for(var i = 0; i < 25; i++) {
+    console.log(ROTnCipher.decode(string, i));
+  }
+};
+allencoders('i am just kidding. you are wonderful');
--- test/spec/rot13_spec.js
+++ test/spec/rot13_spec.js
@@ -1,10 +1,71 @@
 'use strict';
 
 describe('ROT13Cipher', function () {
+  var original = 'hello hello';
+  var updated ='uryyb uryyb';
+  var cipher = ROT13Cipher;
+
   describe('.encode', function () {
-    // write your test here
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.encode(original)).toEqual(updated);
+    });
   });
-  describe('.decode', function () {
-    // write your test here
+
+ describe('.decode', function () {
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.decode(updated)).toEqual(original);
+    });
+  });
+});
+
+
+describe('ROT25Cipher', function () {
+  var original = 'hello hello';
+  var updated ='gdkkn gdkkn';
+  var cipher = ROT25Cipher;
+
+  describe('.encode', function () {
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.encode(original)).toEqual(updated);
+    });
+  });
+
+ describe('.decode', function () {
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.decode(updated)).toEqual(original);
+    });
+  });
+});
+
+describe('ROTnCipher', function () {
+  var original = 'hello hello';
+  var updated1 = 'uryyb uryyb'
+  var updated2 ='gdkkn gdkkn';
+  var cipher = ROTnCipher;
+
+  describe('.encode', function () {
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.encode(original, 0)).toEqual(original);
+    });
+  });
+  describe('.encode', function () {
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.encode(original, 13)).toEqual(updated1);
+    });
+  });
+  describe('.encode', function () {
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.encode(original, 25)).toEqual(updated2);
+    });
+  });
+ describe('.decode', function () {
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.decode(updated1, 13)).toEqual(original);
+    });
+  });
+ describe('.decode', function () {
+    it('returns the string coded in the caesar cipher', function () {
+      expect(cipher.decode(updated2, 25)).toEqual(original);
+    });
   });
 });
