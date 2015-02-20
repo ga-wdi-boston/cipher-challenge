8516456282ff45263c4486dd1b13de83349ede49
katyf <katy.feng.14@gmail.com>
2015-02-20 09:09:52 -0500

    ROT13 25 and n ciphers

--- test/spec/rot13_spec.js
+++ test/spec/rot13_spec.js
@@ -3,8 +3,45 @@
 describe('ROT13Cipher', function () {
   describe('.encode', function () {
     // write your test here
+    it('encodes a string using the caesar cipher', function() {
+      expect(ROT13Cipher.encode('apple apple')).toEqual('nccyr nccyr');
+    });
   });
   describe('.decode', function () {
     // write your test here
+    it('decodes a string using the caesar cipher', function() {
+      expect(ROT13Cipher.decode('nccyr')).toEqual('apple');
+    });
+  });
+});
+
+describe('ROT25Cipher', function() {
+  var plain = "hello";
+  var coded = "gdkkn";
+
+   describe('.encode', function () {
+    // write your test here
+    it('encodes a string using the caesar cipher', function() {
+      expect(ROT25Cipher.encode(plain)).toEqual(coded);
+    });
+  });
+  describe('.decode', function () {
+    // write your test here
+    it('decodes a string using the caesar cipher', function() {
+      expect(ROT25Cipher.decode(coded)).toEqual(plain);
+    });
+  });
+});
+
+describe('ROTnCipher', function() {
+   describe('.encode', function () {
+    it('encodes a string using the caesar cipher', function() {
+      expect(ROTnCipher.encode('mmm beer', 15)).toEqual("bbb qttg");
+    });
+  });
+  describe('.decode', function () {
+    it('decodes a string using the caesar cipher', function() {
+      expect(ROTnCipher.decode("bbb qttg", 15)).toEqual("mmm beer");
+    });
   });
 });
