7ea2969fd0d29110d2a99997be3f6dfad4d586ab
idelairre <idelairre@gmail.com>
2015-02-20 17:52:17 -0500

    fixed the encode function thanks to Doug's help. The functions behaved as they should have but they weren't being called. Shouldn't be too hard to fix the decode function.

--- app/js/rot13.js
+++ app/js/rot13.js
@@ -1,49 +1,55 @@
 'use strict';
 
 var ROT13Cipher = (function() {
-    var encode = function(stringIn) {
-        var encodedString = [];
+  var encode = function(stringIn) {
+    var encodedString = [];
 
-        var findCharFor = function(string) {
-            for (var i = 0; i < this.string.length; i++) {
-              encodedString.push(this.string.charCodeAt(i));
-            }
-        };
-
-        var changeCharValues = function() {
-            for (var i = 0; i < encodedString.length; i++) {
-                if ((encodedString[i] + 13) > 122) {
-                  encodedString[i] = (encodedString[i] + 13) - 26;
-            } else {
-                encodedString[i] = encodedString[i] + 13;
-            }
-          }
-      };
-
-        var encodeString = function() {
-            for (var i = 0; i < encodedString.length; i++) {
-                encodedString[i] = String.fromCharCode(encodedString[i]);
-            }
-            return encodedString.join("");
-        };
-
-    var decode = function(stingIn){
-        var reverseCharValues = function(string) {
-            for (var i = 0; i < encodedString.length; i++) {
-                if ((encodedString[i] - 13) > 122) {
-                  encodedString[i] = (encodedString[i] - 13) - 26;
-            } else {
-                encodedString[i] = encodedString[i] - 13;
-            }
-          }
-        };
-      };
-
-      return {
-        encode: encode
+    var findCharFor = function(string) {
+      for (var i = 0; i < string.length; i++) {
+        encodedString.push(string.charCodeAt(i));
+      }
     };
+
+    var changeCharValues = function() {
+      for (var i = 0; i < encodedString.length; i++) {
+        if ((encodedString[i] + 13) > 122) {
+          encodedString[i] = (encodedString[i] + 13) - 26;
+        } else {
+          encodedString[i] = encodedString[i] + 13;
+        }
+      }
+    };
+
+    var encodeString = function() {
+      for (var i = 0; i < encodedString.length; i++) {
+        encodedString[i] = String.fromCharCode(encodedString[i]);
+      }
+    };
+    findCharFor(stringIn);
+    changeCharValues();
+    encodeString();
+    return encodedString.join("");
+  };
+
+  var decode = function(stringIn){
+    var reverseCharValues = function(string) {
+      for (var i = 0; i < encodedString.length; i++) {
+        if ((encodedString[i] - 13) > 122) {
+          encodedString[i] = (encodedString[i] - 13) - 26;
+        } else {
+          encodedString[i] = encodedString[i] - 13;
+        }
+      }
+    };
+    reverseCharValues(stringIn);
+  };
+
+  return {
+    encode: encode
+  };
 })();
 
 
-var code = ROT13Cipher.encode("abcdefghijklmnopqrstucvwxyz");
-console.log(code);
+var code = ROT13Cipher;
+console.log(code.encode('hello'));
+console.log(code.decode('uryyb'))
