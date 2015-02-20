6e1c535ecc0b2a9c78d4bf7fa453a0cddb2bf9ce
Dan Stoppelman <dstop75@gmail.com>
2015-02-19 21:59:35 -0500

    working solution - needs refactoring

--- app/js/rot13.js
+++ app/js/rot13.js
@@ -1,5 +1,33 @@
 'use strict';
 
-var ROT13Cipher = (function () {
-  // your code here
+var ROT13Cipher = (function() {
+    var encode = function(string){
+      var array = string.split(''),
+          letters = [],
+          shift, value;
+      for(var i=0; i<string.length; i++){
+       value = array[i].charCodeAt();
+        if (value >= 'A'.charCodeAt() && value <= 'Z'.charCodeAt()){
+          if (value+13 > 'Z'.charCodeAt()){
+           shift = value + 12 - ('Z'.charCodeAt() - 'A'.charCodeAt());
+          }else{
+           shift = value + 13;
+          }
+        }else if(( value >= 'a'.charCodeAt() && value <= 'z'.charCodeAt() )) {
+          if (value+13 > 'z'.charCodeAt()){
+           shift = value + 12 - ('z'.charCodeAt() - 'a'.charCodeAt());
+          }else{
+           shift = value + 13;
+          }
+        }else{
+          shift = value;
+        }
+       letters.push(String.fromCharCode(shift));
+      }
+      return letters.join('');
+    };
+    return{
+      encode: encode,
+      decode: encode
+    };
 })();
