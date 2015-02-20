e56123b27daf630855dc2aac6f8aed84afaa1dd5
idelairre <idelairre@gmail.com>
2015-02-20 10:12:40 -0500

    added a decode function.

--- app/js/rot13.js
+++ app/js/rot13.js
@@ -6,7 +6,7 @@ var ROT13Cipher = (function() {
 
         var findCharFor = function(string) {
             for (var i = 0; i < this.string.length; i++) {
-                encodedString.push(this.string.charCodeAt(i));
+              encodedString.push(this.string.charCodeAt(i));
             }
         };
 
@@ -26,12 +26,24 @@ var ROT13Cipher = (function() {
             }
             return encodedString.join("");
         };
+
+    var decode = function(stingIn){
+        var reverseCharValues = function(string) {
+            for (var i = 0; i < encodedString.length; i++) {
+                if ((encodedString[i] - 13) > 122) {
+                  encodedString[i] = (encodedString[i] - 13) - 26;
+            } else {
+                encodedString[i] = encodedString[i] - 13;
+            }
+          }
+        };
+      };
+
       return {
         encode: encode
     };
 })();
 
-// var decode
 
 var code = ROT13Cipher.encode("abcdefghijklmnopqrstucvwxyz");
 console.log(code);
