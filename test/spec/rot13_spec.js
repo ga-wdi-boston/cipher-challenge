c394192db27f21be91fb0cfce76cf57d90247924
pat <patrick.h.needham@gmail.com>
2015-02-20 09:18:16 -0500

    rot13 works

--- test/spec/rot13_spec.js
+++ test/spec/rot13_spec.js
@@ -2,9 +2,28 @@
 
 describe('ROT13Cipher', function () {
   describe('.encode', function () {
-    // write your test here
+    it("encodes a string into caesar cipher code", function() {
+        expect(ROT13Cipher.Encode('hello').display()).toEqual('uryyb');
+        expect(ROT13Cipher.Encode('pat').display()).toEqual('cng');
+      });
   });
   describe('.decode', function () {
-    // write your test here
+    it("decodes a string into caesar cipher code", function() {
+        expect(ROT13Cipher.Encode('uryyb').display()).toEqual('hello');
+        expect(ROT13Cipher.Encode('cng').display()).toEqual('pat');
+      });
+  });
+});
+
+describe('ROT13Cipher', function () {
+  describe('.encode', function () {
+    it("encodes a string into caesar cipher 25 code", function() {
+        expect(ROT13Cipher.Encode('hello pat').display()).toEqual('uryyb cng');
+      });
+  });
+  describe('.decode', function () {
+    it("decodes a string into caesar cipher code", function() {
+        expect(ROT13Cipher.Encode('uryyb cng').display()).toEqual('hello pat');
+      });
   });
 });
