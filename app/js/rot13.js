dcf0b5f36f937d50a6f6a0d3f02ee6ca9db1bb90
Max Blaushild <blaushmild@gmail.com>
2015-08-03 09:59:45 -0400

    reformats

--- app/js/rot13.js
+++ app/js/rot13.js
@@ -3,21 +3,19 @@
 var CaesarCipher = (function () {
 
   var plain = "abcdefghijklmnopqrstuvwxyz".split("");
+  
+  var encode = function(string, n) {
+    return string.split('').map(function(char) { return plain[(plain.indexOf(char) + n) % 26]}).join('');
+  };
 
+  var decode = function(string, n) {
+    return string.split('').map(function(char) { return plain[(plain.indexOf(char) - n + 26) % 26]}).join('');
+  };
 
-
-    var encode = function(string, n) {
-      return string.split('').map(function(char) { return plain[(plain.indexOf(char) + n) % 26]}).join('');
-    };
-
-    var decode = function(string, n) {
-      return string.split('').map(function(char) { return plain[(plain.indexOf(char) - n + 26) % 26]}).join('');
-    };
-
-    return {
-      encode: encode,
-      decode: decode
-    }
+  return {
+    encode: encode,
+    decode: decode
+  };
 
 })();
 
