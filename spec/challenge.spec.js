cbfca0b223189e516e8c879fd20b16c3e022f34b
MicFin <mrfinneran@gmail.com>
2017-03-01 23:48:05 -0500

    Style updates per linter
    Snakecase variable names and single quote strings

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -7,27 +7,27 @@ const ROT13Cipher = require('../lib/challenge');
 
 describe('ROT13Cipher', function () {
   it('encodes a single word', function () {
-    const message = "rachelwuzheer";
-    const encoded_message = "enpuryjhmurre";
-    expect(ROT13Cipher.encode(message)).to.eql(encoded_message);
+    const message = 'rachelwuzheer';
+    const encodedMessage = 'enpuryjhmurre';
+    expect(ROT13Cipher.encode(message)).to.eql(encodedMessage);
   });
 
   it('encodes multiple words', function () {
-    const message = "rachel wuz heer";
-    const encoded_message = "enpury jhm urre";
-    expect(ROT13Cipher.encode(message)).to.eql(encoded_message);
+    const message = 'rachel wuz heer';
+    const encodedMessage = 'enpury jhm urre';
+    expect(ROT13Cipher.encode(message)).to.eql(encodedMessage);
   });
 
   it('decodes a single word', function () {
-    const encoded_message = 'uryybjbeyq';
-    const decoded_message = 'helloworld';
-    expect(ROT13Cipher.decode(encoded_message)).to.eql(decoded_message);
+    const encodedMessage = 'uryybjbeyq';
+    const decodedMessage = 'helloworld';
+    expect(ROT13Cipher.decode(encodedMessage)).to.eql(decodedMessage);
   });
 
   it('decodes multiple words', function () {
-    const encoded_message = "enpury jhm urre";
-    const decoded_message = "rachel wuz heer";
-    expect(ROT13Cipher.decode(encoded_message)).to.eql(decoded_message);
+    const encodedMessage = 'enpury jhm urre';
+    const decodedMessage = 'rachel wuz heer';
+    expect(ROT13Cipher.decode(encodedMessage)).to.eql(decodedMessage);
   });
 });
 
